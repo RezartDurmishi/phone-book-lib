f2819eafb65127fd8aa9e09662fecbaad777a815
RezartDurmishi <01rezart@gmail.com>
2022-06-23 08:22:12 +0200

    git init

---
+++ src/main/java/pb/lib/Contact.java
@@ -0,0 +1,16 @@
+package pb.lib;
+
+abstract public class Contact {
+    // first and last name
+    private String fullName;
+
+    // Work, Cellphone or Home //todo: enum
+    private String type;
+
+    // Contact no //todo: unique
+    private String number;
+
+    public void create(String fullName, String type, String number){
+
+    }
+}
